91c9eadd9af6636010f97d4d4b416bc1416e2224
Matheus Macedo <matheusmacedo643@gmail.com>
2021-12-18 15:09:39 -0300

    Primeira aplicação ReactJs

---
+++ src/styles/global.ts
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  
+  :root {
+    --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+    
+    --blue-light: #6933FF;
+    
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #f0f2f5
+    --shape: #FFFFFF;
+  }
+
+ 
+
+  
+  * {
+       margin: 0;
+       padding;
+       box-sizing: border-box;
+   }
+    
+   html {
+       @media (max-width: 1080px) {
+           font-size: 93.75%;
+       }
+
+       @media (max-width: 720px) {
+           font-size: 87.5%;
+       }
+   }
+   body {
+       background: var(--background);
+       -webkit-font-smothing: antialiased;
+   }
+
+   body, input, textarea, button { 
+       font-family: 'Poppis', sans-serif;
+       font-weight: 400;
+   }
+
+   h1, h2, h3, h4, h5, h6, strong {
+       font-weight: 600;
+       
+   } 
+  
+   button {
+    cursor: pointer;
+   }
+
+   [disabled] {
+       opacity: 0.6;
+       cursor: not-allowed;
+   }
+
+`
+ 
+  
