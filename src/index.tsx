9294349923855929b1275dac107ee5dff96bd9c7
Matheus Macedo <matheusmacedo@MacBook-Pro-de-Matheus.local>
2022-10-07 11:20:17 -0300

    mudando endpoint api

--- src/index.tsx
+++ src/index.tsx
@@ -8,28 +8,28 @@ createServer({
     transaction: Model,
   },
 
-     seeds(server) {
-       server.db.loadData({
-         transactions: [
-           {
-             id: 1,
-             title: 'Programador Junior',
-             type: 'deposit',
-             category: 'Dev',
-             amount: 2300,
-             createdAt: new Date('2021-02-12 09:00:00'),
-           },
+    seeds(server) {
+      server.db.loadData({
+        transactions: [
           {
-            id: 2,
-            title: 'aluguel',
-            type: 'withdraw',
-            category: 'Casa',
-            amount: 1100,
-            createdAt: new Date('2021-02-14 11:00:00'),
-          }
-         ]
-       })
-     },
+            id: 1,
+            title: 'Programador Junior',
+            type: 'deposit',
+            category: 'Dev',
+            amount: 2300,
+            createdAt: new Date('2021-02-12 09:00:00'),
+          },
+        {
+          id: 2,
+          title: 'aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
+        }
+        ]
+      })
+    },
 
   routes() {
     this.namespace = 'api';
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
 
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://http://planilhasonline.s3-website-sa-east-1.amazonaws.com//api',
 })
