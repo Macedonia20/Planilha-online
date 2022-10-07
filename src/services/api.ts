ff7f593322cfc8bc41ccea4bb72f9146f6d73c26
Matheus Macedo <matheusmacedo@MacBook-Pro-de-Matheus.local>
2022-10-07 12:21:10 -0300

    mudando endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const api = axios.create({
 
-    baseURL: 'http://planilhasonline.s3-website-sa-east-1.amazonaws.comyar/api',
+    baseURL: 'http://planilhasonline.s3-website-sa-east-1.amazonaws.com/api',
 })
