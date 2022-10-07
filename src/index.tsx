aaa5239b9d155c776775c201ad5c24311b37100a
Matheus Macedo <matheusmacedo@MacBook-Pro-de-Matheus.local>
2022-10-07 12:09:40 -0300

    mudando endpoint

--- src/index.tsx
+++ src/index.tsx
@@ -32,7 +32,7 @@ createServer({
     },
 
   routes() {
-    this.namespace = '/api';
+    this.namespace = '/api/';
     
     this.get('/transactions', () => {
      return this.schema.all('transaction')
